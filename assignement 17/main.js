71ef7c0ca692e47f18641b5e4819cde1ad945410
[Farwah Aziz] <[siddiquifarwah@gmail.coml]>
2024-05-28 02:04:13 -0700

    first commit

---
+++ assignement 17/main.js
@@ -0,0 +1,21 @@
+"use strict";
+//Assignment 17 
+let guestlist = ["umer", "Ayesha", "ibtisam"];
+//shrinking our guestlist 
+console.log("unfortunatly our new dinner is not arrived yet so we have to inivite only two peoples");
+guestlist.unshift("alice", "ben");
+console.log("updated list of guestlist", guestlist);
+//removing guest from list
+while (guestlist.length > 2) {
+    let removeguest = guestlist.pop();
+    if (removeguest !== undefined) {
+        console.log(`sorry, ${removeguest} we cant invite you`);
+    }
+}
+//print a message for those who are invited
+guestlist.forEach(invitedguests => {
+    console.log(`dear ${invitedguests}you are still invited for the dinner`);
+});
+//removing list
+guestlist.splice(0, guestlist.length);
+console.log(`updatedlist of guest`, guestlist);
